fed3d4cb805e81427b8aac01c421d910d3ac66d5
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-04-11 22:52:02 +0300

    Created a new service for retrieving scoreboard data from api

---
+++ src/services/scoreboard.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Scoreboard} from '../models/Scoreboard/scoreboard';
+import {AppConstants} from '../components/app/app-constants';
+
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ScoreboardMapper} from '../models/Scoreboard/scoreboard-mapper';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScoreboardService {
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      // 'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/plain'
+      // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+      // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+    }),
+    withCredentials: false
+  };
+
+  // GameDate has date format MM/DD/YYYY
+  // private gameDate: string;
+
+  // For Testing!!
+  private gameDate = '02/14/2018';
+
+  private scoreboardApiUrl = AppConstants.baseNbaApiUrl + `scoreboard?GameDate=${this.gameDate}&LeagueID=00&DayOffset=0`;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getScoreboardData(gameDate): Observable<Array<Scoreboard>> {
+    return this.http.get<Array<Scoreboard>>(this.scoreboardApiUrl, this.httpOptions)
+      .pipe(
+        map(scoreboardArray => ScoreboardMapper.mapToModel(scoreboardArray))
+      );
+  }
+}
