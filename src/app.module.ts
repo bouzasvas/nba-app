e895e8ba6e4c188f3ed15cbf4103b4ccb4d71393
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-04-13 23:47:21 +0300

    Insert components in app module and update template

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './components/app/app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HeaderComponent} from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import { GamesComponent } from './components/games/games.component';
+import { StandingsComponent } from './components/standings/standings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    GamesComponent,
+    StandingsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
