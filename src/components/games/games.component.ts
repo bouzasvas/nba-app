39fc1a061e22c9f0b7b9b8dd4b20f6dbc5ff0069
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-05-02 14:01:57 +0300

    Started Games Component

--- src/components/games/games.component.ts
+++ src/components/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-games',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+  @ViewChild('fromDate') fromDate;
+  @ViewChild('toDate') toDate;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
