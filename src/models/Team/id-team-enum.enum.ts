1e86e4266d0e40d2328fd1fcdf1a40a71269a712
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-05-03 19:07:04 +0300

    Complete Team Id-Name mappings

--- src/models/Team/id-team-enum.enum.ts
+++ src/models/Team/id-team-enum.enum.ts
@@ -1,33 +1,32 @@
 export enum IdTeamEnum {
   'id_1610612737' = 'Atlanta Hawks',
-  'id_1610612738' = 'Boston Celtics'
-// ....
-// Brooklyn Nets	1610612751
-// Charlotte Hornets	1610612766
-// Chicago Bulls	1610612741
-// Cleveland Cavaliers	1610612739
-// Dallas Mavericks	1610612742
-// Denver Nuggets	1610612743
-// Detroit Pistons	1610612765
-// Golden State Warriors	1610612744
-// Houston Rockets	1610612745
-// Indiana Pacers	1610612754
-// Los Angeles Clippers	1610612746
-// Los Angeles Lakers	1610612747
-// Memphis Grizzlies	1610612763
-// Miami Heat	1610612748
-// Milwaukee Bucks	1610612749
-// Minnesota Timberwolves	1610612750
-// New Orleans Pelicans	1610612740
-// New York Knicks	1610612752
-// Oklahoma City Thunder	1610612760
-// Orlando Magic	1610612753
-// Philadelphia 76ers	1610612755
-// Phoenix Suns	1610612756
-// Portland Trail Blazers	1610612757
-// Sacramento Kings	1610612758
-// San Antonio Spurs	1610612759
-// Toronto Raptors	1610612761
-// Utah Jazz	1610612762
-// Washington Wizards	1610612764
+  'id_1610612738' = 'Boston Celtics',
+  'id_1610612751' = 'Brooklyn Nets',
+  'id_1610612766' = 'Charlotte Hornets',
+  'id_1610612741' = 'Chicago Bulls',
+  'id_1610612739' = 'Cleveland Cavaliers',
+  'id_1610612742' = 'Dallas Mavericks',
+  'id_1610612743' = 'Denver Nuggets',
+  'id_1610612765' = 'Detroit Pistons',
+  'id_1610612744' = 'Golden State Warriors',
+  'id_1610612745' = 'Houston Rockets',
+  'id_1610612754' = 'Indiana Pacers',
+  'id_1610612746' = 'Los Angeles Clippers',
+  'id_1610612747' = 'Los Angeles Lakers',
+  'id_1610612763' = 'Memphis Grizzlies',
+  'id_1610612748' = 'Miami Heat',
+  'id_1610612749' = 'Milwaukee Bucks',
+  'id_1610612750' = 'Minnesota Timberwolves',
+  'id_1610612740' = 'New Orleans Pelicans',
+  'id_1610612752' = 'New York Knicks',
+  'id_1610612760' = 'Oklahoma City Thunder',
+  'id_1610612753' = 'Orlando Magic',
+  'id_1610612755' = 'Philadelphia 76ers',
+  'id_1610612756' = 'Phoenix Suns',
+  'id_1610612757' = 'Portland Trail Blazers',
+  'id_1610612758' = 'Sacramento Kings',
+  'id_1610612759' = 'San Antonio Spurs',
+  'id_1610612761' = 'Toronto Raptors',
+  'id_1610612762' = 'Utah Jazz',
+  'id_1610612764' = 'Washington Wizards'
 }
