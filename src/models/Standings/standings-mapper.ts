4069ac43808e07fc444c388d7c2500491ffb3ff5
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-05-03 23:19:16 +0300

    Fixes from refactoring models

--- src/models/Standings/standings-mapper.ts
+++ src/models/Standings/standings-mapper.ts
@@ -12,7 +12,14 @@ export class StandingsMapper {
     standingsResponse.forEach(teamStanding => {
       const standing = {} as StandingsPerTeam;
 
-      const team: Team = {id: teamStanding[2], teamCity: teamStanding[3], teamName: teamStanding[4], fullName: `${teamStanding[3]} ${teamStanding[4]}`};
+      const team: Team = {
+        id: teamStanding[2],
+        teamCity: teamStanding[3],
+        teamName: teamStanding[4],
+        fullName: `${teamStanding[3]} ${teamStanding[4]}`,
+        teamLogoPath: `assets/nba-teams-logo/${teamStanding[4]}.gif`,
+        pointsScored: 0
+      };
       standing.team = team;
       standing.conference = teamStanding[5];
       standing.gamesWin = teamStanding[12];
