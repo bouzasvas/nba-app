670b5a3a65f57896d2349737e2a779400bef0ec4
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-05-05 12:47:21 +0300

    Fixed proxy issue!!!

--- src/common/app-constants.ts
+++ src/common/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
-  public static baseNbaApiUrl = 'https://stats.nba.com/stats';
-  // public static baseNbaApiUrl = '/stats/';
+  // public static baseNbaApiUrl = 'https://stats.nba.com/stats';
+  public static baseNbaApiUrl = '/stats';
 }
