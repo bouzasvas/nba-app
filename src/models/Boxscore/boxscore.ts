d84910d4ba722dabc02592c6e75b0fc4fa24bad5
Vassilis Bouzas <vassilisbouzas@gmail.com>
2019-06-29 16:34:45 +0300

    Added new models for Game Boxscore

---
+++ src/models/Boxscore/boxscore.ts
@@ -0,0 +1,5 @@
+import { PlayerGameStats } from '../Player/player-game-stats';
+
+export interface Boxscore {
+  boxscorePlayersStats: Array<PlayerGameStats>;
+}
---
+++ src/models/Player/player-game-stats.ts
@@ -0,0 +1,25 @@
+export interface PlayerGameStats {
+  id: string;
+  name: string;
+  position: string;
+  minutes: string;
+  fgm: number;
+  fga: number;
+  fg_pct: number;
+  fg3m: number;
+  fg3a: number;
+  fg3_pct: number;
+  ftm: number;
+  fta: number;
+  ft_pct: number;
+  offensiveRebounds: number;
+  defensiveRebounds: number;
+  totalRebounds: number;
+  assists: number;
+  steals: number;
+  blocks: number;
+  turnovers: number;
+  personalFouls: number;
+  pointsScored: number;
+  plusMinus: number;
+}
