72e8daf1dc158e67657bbd1b33c4a41813b5c8a1
Vassilis Bouzas <bill_bouzas@hotmail.com>
2019-07-04 23:56:19 +0300

    Game Highlights Components
    - Service call for Retrieving Game Highlights, Template & CSS

--- src/components/game-highlights/game-highlights.component.ts
+++ src/components/game-highlights/game-highlights.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LoaderService} from '../../services/loader.service';
+import {YoutubeApiService} from '../../services/youtube-api.service';
+import {YoutubeApiSearchResults} from '../../models/YoutubeApi/youtube-api-search-results';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-game-highlights',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameHighlightsComponent implements OnInit {
 
-  constructor() { }
+  @Input() homeTeam: string;
+  @Input() awayTeam: string;
+  @Input() gameDate: string;
+
+  gameHighlights: Array<YoutubeApiSearchResults>;
+
+  constructor(private loader: LoaderService, private youtubeApi: YoutubeApiService) { }
 
   ngOnInit() {
+    this.getGameHighlights();
   }
 
+  private getGameHighlights(): void {
+    this.loader.toggleLoader();
+
+    this.youtubeApi
+      .getVideosBySearchTerm(this.constructSearchTerm())
+      .subscribe(youtubeVideos => {
+        this.gameHighlights = youtubeVideos;
+
+        this.loader.toggleLoader();
+      });
+  }
+
+  private constructSearchTerm() {
+    const gameDateMomentOb = moment(Number.parseFloat(this.gameDate));
+    const formattedGameDateStr = gameDateMomentOb.format('MMMM D, YYYY');
+
+    return `${this.awayTeam} vs ${this.homeTeam} ${formattedGameDateStr}`;
+  }
 }
