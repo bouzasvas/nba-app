e668654dd091ebaf6499b97118b1c8630f8b70ee
Bill Bouzas <bill_bouzas@hotmail.com>
2019-11-17 16:28:08 +0200

    Fix getSessionYear function implementation based on current season month

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,13 +1,24 @@
 export class Utils {
 
   public static getCurrentSeasonYear(): string {
-    const d = new Date();
-
-    const currentYear = d.getFullYear();
+    const currentYear = Utils.getCurrentYear();
     const nextYear = currentYear + 1;
 
     // Convert nextYear to Str
     const nextYearStr = String(nextYear);
     return `${currentYear}-${nextYearStr.substring(2)}`;
+  }
+
+  public static getCurrentYear(): number {
+    const d = new Date();
+
+    let currentYear = d.getFullYear();
+    const currentMonth = d.getMonth();
+
+    if (currentMonth < 8) {
+      currentYear = currentYear - 1;
+    }
+
+    return currentYear;
   }
 }
