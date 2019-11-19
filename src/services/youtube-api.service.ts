c1afcdd33a790847dee84471efc5ccd01624fa8d
Bill Bouzas <bill_bouzas@hotmail.com>
2019-11-19 15:29:18 +0200

    Remove youtube api key & environment.prod file from Git

--- src/services/youtube-api.service.ts
+++ src/services/youtube-api.service.ts
@@ -1,25 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {LoggerService} from './logger.service';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {YoutubeApiSearchResults} from '../models/YoutubeApi/youtube-api-search-results';
 import {HttpClient} from '@angular/common/http';
-import {map, tap} from "rxjs/operators";
+import {map, tap} from 'rxjs/operators';
 import {YoutubeApiMapper} from '../models/YoutubeApi/youtube-api-mapper';
+
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeApiService {
 
-  private youtubeApiKey = 'AIzaSyBlUik7ZD91Ko84M32GEUx-P29Hmo6eqmA';
+  private youtubeApiKey = environment.youtubeApiKey;
 
   private baseYoutubeApiUrl = 'https://www.googleapis.com/youtube/v3';
   private searchOperationUrl = (searchTerm) => `/search?part=snippet&q=${searchTerm}&type=video&key=${this.youtubeApiKey}`;
 
-  constructor(private http: HttpClient, private logger: LoggerService, private youtubeApiMapper: YoutubeApiMapper) { }
+  constructor(private http: HttpClient, private logger: LoggerService, private youtubeApiMapper: YoutubeApiMapper) {
+  }
 
   getVideosBySearchTerm(searchTerm: string): Observable<Array<YoutubeApiSearchResults>> {
     const endpointUrl = this.baseYoutubeApiUrl + this.searchOperationUrl(searchTerm);
+
+    // If Youtube API Key is not defined (dev) return an observable of empty array
+    if (!this.youtubeApiKey) {
+      this.logger.warn('Videos not Retrieved because API Key is missing');
+      return of([]);
+    }
 
     return this.http
       .get(endpointUrl)
